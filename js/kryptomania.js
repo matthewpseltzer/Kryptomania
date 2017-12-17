c536e908f6cde7589f9d36d43f03b8324f21bfc7
Matthew P. Seltzer <matthewpseltzer@gmail.com>
2017-12-17 00:42:50 -0500

    Add files via upload
    Commit leading up to final commit for team project 1.

--- js/kryptomania.js
+++ js/kryptomania.js
@@ -1,111 +1,111 @@
 // Matthew P. Seltzer
 // Dec. 12, 2017
-// kryptomania.js
-"use strict";
-var currentQuestion = 0;
-var correctAnswers = [1,
-                      2,
-                      0];
-var responses = ["","",""];
-
-//var startBtn = document.getElementById("startBtn");
-var quizQuestions = ["The mother of Superman's son is ",
-                     "During his Superboy career in Smallville, Superman's secret identity was known by ",
-                     "Supergirl's adoptive parents were "];
-var choices =       [["Lori Lemaris","Lois Lane","Lana Lang","Luma Lynai"],
-                     ["Lana Lang","Lex Luthor","Pete Ross","Bruce Wayne"],
-                     ["Zor-El and Alura", "Jor-El and Lara", "Van-Zee and Sylvia", "Fred and Edna Danvers"]
-                    ];
-// syntax to retrieve values from choices two-dimensional array is choices[i][j], 
-// where, in this case, (i >= 0 and <= quizQuestions.length - 1) and (j >=0 and j < 4)
-
-
+// kryptomania_bu.js
+//"use strict";
+// global variables
+var currentQuestion;
+var correctAnswers = [];
+var responses = [];
+var quizQuestions = [];
+var choices = [];
+var labels = document.getElementsByClassName("choiceText");
+var radios = document.getElementsByClassName("choice");
+// end global variables
 
 function setUpQuiz(){
+    correctAnswers = [1,
+                      2,
+                      3];
+    
+    quizQuestions = ["The mother of Superman's son is ",
+                     "During his Superboy career in Smallville, Superman's secret identity was known by ",
+                     "Supergirl's adoptive parents were "];
+    choices =       [["Lori Lemaris","Lois Lane","Lana Lang","Luma Lynai"],
+                     ["Lana Lang","Lex Luthor","Pete Ross","Bruce Wayne"],
+                     ["Zor-El and Alura", "Jor-El and Lara", "Van-Zee and Sylvia", "Fred and Edna Danvers"]
+                    ]; 
+
+    // syntax to retrieve values from choices two-dimensional array is choices[i][j], 
+    // where, in this case, (i >= 0 and <= quizQuestions.length - 1) and (j >=0 and j < 4)
+
     var quiz = document.getElementById("quiz");
     quiz.style.display = "block";
-    /*
-    for (var i= 0; i < quizQuestions.length - 1; i++){
-    responses.push(-1);
-    */
+    currentQuestion = 0;
+    clearSelections();  
     refreshContent(); 
+   
 }
-
-
+/*
 function isChecked(){
-    var isChecked; // Boolean value
-    var radios = document.getElementsByClassName("choices");
     for(var i = 0; i < radios.length - 1; i++){
-        if (radios[i].checked) {
-           isChecked = true;
-           updateResponse(i);
+        if (radios[i].checked === true) {
+           currentResponse = i;
            return true;
         }        
-    }
-    if (!isChecked){
-        window.alert("Please make a choice before continuing.");
-        return false;
-    }
+        else
+        {
+            return false;
+        }
+    }    
 }
-
+*/
 function moveNext(){
-    // Checks to see if this is the last question.
-    if (isEndOfQuiz && (!isChecked())) {
-       window.alert("This is the last question.  Please make a selection.");
+    if (currentQuestion < quizQuestions.length){
+        setResponse();         
+        currentQuestion += 1; 
+        if (currentQuestion < quizQuestions.length){
+            clearSelections();
+            refreshContent();
+        }          
     }
-    else if (isEndOfQuiz && (isChecked()))
+    else 
     {
-        window.alert("Congratulations!  You've made it to the end!");
+        window.alert("Congratulations!  You've reached the end of the quiz!  Your score is " + score() + ".");
     }
-    else if (!isEndOfQuiz)
-    {  
-      isChecked();
-      currentQuestion += 1;
-      refreshContent();
-    }
-    
-}
 
+} 
+    
 // updates radio button label content and text of quiz question
 function refreshContent(){
-    var labels = document.getElementsByClassName("description");
-    for (var j = 0; j <= labels.length - 1; j++){
-        labels[j].innerHTML = choices[currentQuestion][j];
-        console.log(choices[currentQuestion][j])
-    }
-    document.getElementById("questionNumberAndText").innerHTML = (currentQuestion + 1) + ". " + quizQuestions[currentQuestion];    
+    if (currentQuestion < quizQuestions.length){
+        document.getElementById("questionNumberAndText").innerHTML = (currentQuestion + 1) + ". " + quizQuestions[currentQuestion];    
+        for (var j = 0; j <= labels.length - 1; j++){
+            labels[j].innerHTML = choices[currentQuestion][j];       
+        }
+    }      
+           
 }
 
-function updateResponse(i){
-  responses[currentQuestion] = i;
+function setResponse(){
+    for(var i = 0; i < radios.length - 1; i++){
+        if (radios[i].checked === true) {
+           responses.push(i);
+        }
+    }       
 }
 
-function isEndOfQuiz(){
-    if((currentQuestion + 1) === responses.length){
-        return true;
+function clearSelections(){
+    for (var i = 0; i < radios.length -1; i++){
+        radios[i].checked = false;
     }
-    else
-    {
-        return false;
-    }
-}
 
+}
 function createEventListeners(){
-    var supermanBtn  = document.getElementById("supermanBtn");
-    if (supermanBtn.addEventListener){
-        supermanBtn.addEventListener("click",moveNext, false);
+    var next = document.getElementById("next");
+    if (next.addEventListener){
+        next.addEventListener("click", moveNext, false);
     }
-    if (window.addEventListener){
-        window.addEventListener("load", setUpQuiz,false);
-    }    
+}
+
+function score(){
+    var numberCorrect = 0;
+    for(var i = 0; i < quizQuestions.length; i++){
+        if (responses[i] === correctAnswers[i]){
+            numberCorrect += 1;
+        }
+    }
+    return ((1.0 * numberCorrect)/(1.0 * quizQuestions.length) * 100.0).toFixed(1) + "%";
 }
 
 window.addEventListener("load", createEventListeners, false);
-
-
-
-
-
-
-
-
+window.addEventListener("load", setUpQuiz, false);
